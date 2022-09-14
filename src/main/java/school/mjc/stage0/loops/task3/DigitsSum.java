3a212e08246b5f31e225ad9dbab220d0545c0d3a
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-09-14 16:26:10 +0500

    Update DigitsSum.java

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
-
+        String str = String.valueOf(t);
+        int len = str.length();
+        int sum = 0;
+        for (int i = 1; i <= len; i++) {
+            int temp = t / (10);
+            int given = t % (10);
+            sum += temp;
+            temp = given;
+        }
+        System.out.println(sum);
     }
 }
