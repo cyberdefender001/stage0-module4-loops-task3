524d637e72b9a4e7c1921f091c15b397b9afc3ea
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-09-14 17:42:39 +0500

    Update AlphabetPrinter.java

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -4,7 +4,7 @@ public class AlphabetPrinter {
     public void printAlphabet() {
         char[] charLetters = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
         for (int i = 0; i < charLetters.length(); i++) {
-            System.out.println(charLetter[i]);
+            System.out.println(charLetters[i]);
         }
     }
 }
