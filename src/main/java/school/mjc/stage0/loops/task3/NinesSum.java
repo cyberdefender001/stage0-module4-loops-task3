f3880180af0456819a68793bb96cf6439cbd4636
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-09-15 14:50:23 +0500

    Update NinesSum.java

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -4,7 +4,7 @@ public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
         int sum = 0;
         int temp = 9;
-        for(int i = 1; i <= lengthOfLastNumber; i++) {
+        for(int i = 0; i <= lengthOfLastNumber; i++) {
             sum += temp;
             temp = temp + i * 90;
         }
